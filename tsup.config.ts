d164b7adbcc83d05d147db388b59b87bd6120a71
rx-ted <1552488649@qq.com>
2024-09-04 13:32:40 +0800

    version: 1.0.2

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/theme.ts'],
-  outDir: path.resolve(__dirname, './dist'),
-  format: ['cjs', 'esm'],  
+  outDir: path.resolve(__dirname, '.'),
+  format: ['cjs',],
   dts: true,
   external: ['vitepress'],
   noExternal: ['vitepress-plugin-tabs'],
